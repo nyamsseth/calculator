d507e2ea242067406713e05d73e195aefbf32e66
nyamsseth <nyamsounds@gmail.com>
2023-11-20 21:12:12 +0300

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,144 +1,169 @@
+package calculator;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Stack;
 
-public class Calculator extends JFrame {
-    private JTextField operationTextField;
-    private JRadioButton convertToPostfixRadioButton;
-    private JButton calculateButton;
+public class AdvancedCalculator extends JFrame {
+    private JTextField inputField;
+    private JButton addButton, subtractButton, multiplyButton, divideButton, sinButton, cosButton, equalButton;
+    private double currentValue = 0.0; // For storing intermediate values
+    private String currentOperation = "";
 
-    public Calculator() {
-        setTitle("Infix and Postfix Calculator");
+    public AdvancedCalculator() {
+        setTitle("Advanced Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 200);
-        setLayout(new GridLayout(3, 1));
+        setSize(300, 400);
 
-        operationTextField = new JTextField("Enter the operation", 20);
-        convertToPostfixRadioButton = new JRadioButton("Convert to Postfix");
-        calculateButton = new JButton("Calculate");
+        // Initialize components
+        inputField = new JTextField(15);
+        addButton = new JButton("+");
+        subtractButton = new JButton("-");
+        multiplyButton = new JButton("*");
+        divideButton = new JButton("/");
+        sinButton = new JButton("sin");
+        cosButton = new JButton("cos");
+        equalButton = new JButton("=");
 
-        Font font = new Font("Arial", Font.BOLD, 20);
-        operationTextField.setFont(new Font("Arial", Font.PLAIN, 20));
-        convertToPostfixRadioButton.setFont(font);
-        calculateButton.setFont(font);
-        operationTextField.setBackground(new Color(240, 240, 240));
-        convertToPostfixRadioButton.setBackground(new Color(240, 240, 240));
-        calculateButton.setBackground(Color.CYAN);
+        // Set layout
+        setLayout(new GridLayout(4, 3));
 
-        convertToPostfixRadioButton.addActionListener(e -> {
-            // Action for radio button selection (if needed)
+        // Add components to the frame
+        add(inputField);
+        add(addButton);
+        add(subtractButton);
+        add(multiplyButton);
+        add(divideButton);
+        add(sinButton);
+        add(cosButton);
+        add(equalButton);
+
+        // Set up action listeners for buttons
+
+        // Operation buttons
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation("+");
+            }
         });
 
-        calculateButton.addActionListener(e -> calculateExpression());
+        subtractButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation("-");
+            }
+        });
 
-        add(operationTextField);
-        add(convertToPostfixRadioButton);
-        add(calculateButton);
+        multiplyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation("*");
+            }
+        });
 
-        setSize(500, 300);
+        divideButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation("/");
+            }
+        });
+
+        // Sine button
+        sinButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Get the current value from the text field
+                    double currentValue = Double.parseDouble(inputField.getText());
+
+                    // Calculate the sine value of the current value
+                    double result = Math.sin(currentValue);
+
+                    // Update the input field with the result
+                    inputField.setText(String.valueOf(result));
+                } catch (NumberFormatException ex) {
+                    // Handle the case where the input is not a valid number
+                    inputField.setText("Invalid input");
+                }
+            }
+        });
+
+        // Cosine button
+        cosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Get the current value from the text field
+                    double currentValue = Double.parseDouble(inputField.getText());
+
+                    // Calculate the cosine value of the current value
+                    double result = Math.cos(currentValue);
+
+                    // Update the input field with the result
+                    inputField.setText(String.valueOf(result));
+                } catch (NumberFormatException ex) {
+                    // Handle the case where the input is not a valid number
+                    inputField.setText("Invalid input");
+                }
+            }
+        });
+
+        // Equal button
+        equalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double value2 = Double.parseDouble(inputField.getText());
+                    double result = 0;
+                    switch (currentOperation) {
+                        case "+":
+                            result = currentValue + value2;
+                            break;
+                        case "-":
+                            result = currentValue - value2;
+                            break;
+                        case "*":
+                            result = currentValue * value2;
+                            break;
+                        case "/":
+                            if (value2 != 0) {
+                                result = currentValue / value2;
+                            } else {
+                                inputField.setText("Division by zero error");
+                                return;
+                            }
+                            break;
+                    }
+                    inputField.setText(String.valueOf(result));
+                    currentValue = result;
+                } catch (NumberFormatException ex) {
+                    inputField.setText("Invalid input");
+                }
+            }
+        });
+
+        // Set the frame visible
         setVisible(true);
     }
 
-    private void calculateExpression() {
-        String exp = operationTextField.getText();
-        String postfixExpression = infixToPostfix(exp);
-
+    // Method to perform arithmetic operations
+    private void performOperation(String operation) {
         try {
-            if (convertToPostfixRadioButton.isSelected()) {
-                double result = evaluatePostfix(postfixExpression);
-                operationTextField.setText("Infix Expression: " + exp +
-                        "\n Postfix Expression: " + postfixExpression +
-                        "\n Result: " + result);
-            }
-        } catch (Exception ex) {
-            operationTextField.setText("Error: " + ex.getMessage());
-            // ex.printStackTrace();
-        }
-    }
-
-    private static int precedence(char operator) {
-        if (operator == '+' || operator == '-') {
-            return 1;
-        } else if (operator == '*' || operator == '/') {
-            return 2;
-        } else {
-            return 0;
-        }
-    }
-
-    public static String infixToPostfix(String infix) {
-        StringBuilder postfix = new StringBuilder();
-        Stack<Character> operatorStack = new Stack<>();
-
-        for (int i = 0; i < infix.length(); i++) {
-            char token = infix.charAt(i);
-
-            if (Character.isDigit(token)) {
-                postfix.append(token);
-            } else if (token == '+' || token == '-' || token == '*' || token == '/') {
-                while (!operatorStack.isEmpty() && operatorStack.peek() != '(' &&
-                        precedence(operatorStack.peek()) >= precedence(token)) {
-                    postfix.append(operatorStack.pop());
-                }
-                operatorStack.push(token);
-            } else if (token == '(') {
-                operatorStack.push(token);
-            } else if (token == ')') {
-                while (operatorStack.peek() != '(') {
-                    postfix.append(operatorStack.pop());
-                }
-                operatorStack.pop();
-            }
-        }
-
-        while (!operatorStack.isEmpty()) {
-            postfix.append(operatorStack.pop());
-        }
-
-        return postfix.toString();
-    }
-
-    public static double evaluatePostfix(String postfixExpression) {
-        Stack<Double> operandStack = new Stack<>();
-
-        for (char c : postfixExpression.toCharArray()) {
-            if (Character.isDigit(c)) {
-                operandStack.push((double) (c - '0'));
-            } else {
-                double operand2 = operandStack.pop();
-                double operand1 = operandStack.pop();
-
-                switch (c) {
-                    case '+':
-                        operandStack.push(operand1 + operand2);
-                        break;
-                    case '-':
-                        operandStack.push(operand1 - operand2);
-                        break;
-                    case '*':
-                        operandStack.push(operand1 * operand2);
-                        break;
-                    case '/':
-                        if (operand2 != 0) {
-                            operandStack.push(operand1 / operand2);
-                        } else {
-                            throw new ArithmeticException("Error: Division by zero");
-                        }
-                        break;
-                }
-            }
-        }
-
-        if (!operandStack.isEmpty()) {
-            return operandStack.pop();
-        } else {
-            throw new IllegalArgumentException("Error: Empty operand stack");
+            currentValue = Double.parseDouble(inputField.getText());
+            currentOperation = operation;
+            inputField.setText("");
+        } catch (NumberFormatException ex) {
+            inputField.setText("Invalid input");
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(PostfixStandardFix::new);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new AdvancedCalculator();
+            }
+        });
     }
 }
